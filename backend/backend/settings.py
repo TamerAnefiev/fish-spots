cb10130b083c1427159b5764c9d5b77f54c08722
Tamer <tamerchoyy@gmail.com>
2025-12-07 19:04:40 +0200

    update because vite starts on 5173. CRA was using 3000

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -178,11 +178,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://127.0.0.1:5173",
     "http://localhost:8000",
 ]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://localhost:8000"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:5173", "http://localhost:8000"]
 
 AUTH_USER_MODEL = "users.CustomUser"
 
