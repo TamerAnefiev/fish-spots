eb2a845ae81114c8567a38befc35107e6253ca76
Tamer <tamerchoyy@gmail.com>
2025-12-23 16:29:45 +0200

    Added context consumer hooks

---
+++ frontend/src/hooks/useCookieConsentContext.ts
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { CookieConsentContext } from "../context/CookieConsentContext";
+
+export const useCookieConsentContext = () => {
+  const context = useContext(CookieConsentContext);
+  if (context === undefined) {
+    throw new Error(
+      "useCookieConsentContext must be used within a CookieConsentProvider"
+    );
+  }
+
+  return context;
+};
