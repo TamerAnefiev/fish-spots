ca118d112ee71de9df5c4ab99dda1fa6afbf9af7
Tamer <tamerchoyy@gmail.com>
2025-12-23 16:30:39 +0200

    Left only the contexts in the file. Moved the provider and the hook to separate files

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,5 +1,4 @@
-import React, { useState, createContext, useEffect, useContext } from "react";
-import { baseUrl } from "../util/constants";
+import { createContext } from "react";
 
 type AuthData = {
   userId: string;
@@ -11,77 +10,4 @@ type AuthData = {
   resetUser: () => void;
 };
 
-const AuthContext = createContext<AuthData | undefined>(undefined);
-
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [userId, setUserId] = useState("");
-  const [user, setUser] = useState("");
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [isLogged, setIsLogged] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const authorize = async () => {
-      try {
-        const options: RequestInit = {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        };
-        const response = await fetch(`${baseUrl}/api/authorize/`, options);
-        const data = await response.json();
-
-        if (response.status === 200) {
-          handleSetUser(data.user, data.id, data.admin);
-        } else if (response.status === 401) {
-          resetUser();
-        }
-      } catch (error) {
-        console.error("authentication error");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    authorize();
-  }, []);
-
-  const handleSetUser = (username: string, id: string, isAdmin: boolean) => {
-    setUser(username);
-    setUserId(id);
-    setIsAdmin(isAdmin);
-    setIsLogged(true);
-  };
-
-  const resetUser = () => {
-    setUser("");
-    setUserId("");
-    setIsAdmin(false);
-    setIsLogged(false);
-  };
-
-  let contextData = {
-    userId,
-    user,
-    isAdmin,
-    isLogged,
-    loading,
-    handleSetUser,
-    resetUser,
-  };
-
-  return (
-    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
-  );
-};
-
-export const useAuthContext = () => {
-  const context = useContext(AuthContext);
-  if (context === undefined) {
-    throw new Error("useAuthContext must be used within a AuthProvider");
-  }
-
-  return context;
-};
+export const AuthContext = createContext<AuthData | undefined>(undefined);
--- frontend/src/context/CookieConsentContext.tsx
+++ frontend/src/context/CookieConsentContext.tsx
@@ -1,78 +1,10 @@
-import React, { useState, createContext, useEffect, useContext } from "react";
-import { baseUrl } from "../util/constants";
+import { createContext } from "react";
 
 type CookieConsentData = {
   hasAgreed: boolean;
   onCookieConsent: (isAccepted: boolean) => void;
 };
 
-const CookieConsentContext = createContext<CookieConsentData | undefined>(
-  undefined
-);
-
-export const CookieConsentProvider = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  const [hasAgreed, setHasAgreed] = useState(true);
-
-  useEffect(() => {
-    const checkCookieConsent = () => {
-      const options: RequestInit = {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-      };
-      fetch(`${baseUrl}/profile/cookie-consent/`, options)
-        .then((response) => response.json())
-        .then((data) => {
-          setHasAgreed(data.consent);
-        })
-        .catch((error) => console.error(error.message));
-    };
-
-    checkCookieConsent();
-  }, []);
-
-  const onCookieConsent = (isAccepted: boolean) => {
-    const options: RequestInit = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ consent: isAccepted }),
-      credentials: "include",
-    };
-    fetch(`${baseUrl}/profile/cookie-consent/`, options)
-      .then((response) => response.json())
-      .then((data) => {
-        setHasAgreed(data.consent);
-      })
-      .catch((error) => console.error(error.message));
-  };
-
-  let contextData = {
-    hasAgreed,
-    onCookieConsent,
-  };
-
-  return (
-    <CookieConsentContext.Provider value={contextData}>
-      {children}
-    </CookieConsentContext.Provider>
-  );
-};
-
-export const useCookieConsentContext = () => {
-  const context = useContext(CookieConsentContext);
-  if (context === undefined) {
-    throw new Error(
-      "useCookieConsentContext must be used within a CookieConsentProvider"
-    );
-  }
-
-  return context;
-};
+export const CookieConsentContext = createContext<
+  CookieConsentData | undefined
+>(undefined);
