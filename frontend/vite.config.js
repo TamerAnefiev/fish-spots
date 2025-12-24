ae875ad66b04b97c94746cf5039435fef13be4e8
Tamer <tamerchoyy@gmail.com>
2025-12-24 09:13:40 +0200

    Added shadcn setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "build", // CRA's default build output
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
