6338f30ee163845653336af60613ddce3ec6c89c
Tamer <tamerchoyy@gmail.com>
2025-12-25 15:45:06 +0200

    Update imports and added ThemeProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,19 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "@/index.css";
 import App from "@/App";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "@/providers/AuthProvider";
-import { CookieConsentProvider } from "@/providers/CookieConsentProvider";
+import { AuthProvider } from "@/context/AuthContext";
+import { CookieConsentProvider } from "@/context/CookieConsentContext";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <CookieConsentProvider>
     <AuthProvider>
       <BrowserRouter>
-        <App />
+        <ThemeProvider defaultTheme="system" storageKey="ui-theme">
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </AuthProvider>
   </CookieConsentProvider>
