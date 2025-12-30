f7e3632a206afadad10a0b842954e552cd57f10d
Tamer <tamerchoyy@gmail.com>
2025-12-30 18:16:58 +0200

    Added theme responsivenvess

--- frontend/src/components/Home/CityCard.tsx
+++ frontend/src/components/Home/CityCard.tsx
@@ -13,21 +13,21 @@ export default function CityCard({
 }: CityCardProps) {
   return (
     <Link to={linkTo}>
-      <section className="hover:bg-cyan-50 max-w-lg bg-gray-200 p-8 rounded-3xl hover:scale-105 duration-150 cursor-pointer shadow-xl">
-        <h3 className="text-2xl text-center font-bold mb-6 relative">
+      <section className="bg-muted max-w-lg cursor-pointer rounded-3xl p-8 shadow-xl duration-150 hover:scale-102">
+        <h3 className="relative mb-6 text-center text-2xl font-bold">
           {cityName}
-          <div className="absolute top-[100%] left-2/4 -translate-x-2/4 -translate-y-1/2 w-16 h-0.5 bg-cyan-600"></div>
+          <div className="absolute top-full left-2/4 h-0.5 w-16 -translate-x-2/4 -translate-y-1/2 bg-cyan-600"></div>
         </h3>
         <div className="flex justify-center gap-x-12">
           <section className="text-2xl">
             {cityData.map((data) => (
               <p
                 key={data}
-                className="mb-3 inline-flex justify-between items-center w-full"
+                className="mb-3 inline-flex w-full items-center justify-between"
               >
                 {data}
                 <span>
-                  <i className="fa-solid fa-check text-white bg-green-500 p-1.5 rounded-full w-8 h-8"></i>
+                  <i className="fa-solid fa-check h-8 w-8 rounded-full bg-green-500 p-1.5 text-white"></i>
                 </span>
               </p>
             ))}
