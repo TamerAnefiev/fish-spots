a575817060605bc0b7955c3faf93f17223ef575b
Tamer <tamerchoyy@gmail.com>
2026-01-08 14:41:42 +0200

    refactor: removed signals

--- backend/chepareta/apps.py
+++ backend/chepareta/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class CheparetaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "chepareta"
-
-    def ready(self):
-        from . import signals
