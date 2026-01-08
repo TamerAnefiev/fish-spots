687a68ea6121ee3c958c567af53ef2001027923c
Tamer <tamerchoyy@gmail.com>
2026-01-08 14:43:44 +0200

    refactor: Instead of signals we manually handle the cleanup of files now.

--- backend/chepareta/models.py
+++ backend/chepareta/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 import os
 
@@ -8,6 +9,12 @@ from .utils import image_directory_distributor
 class Seller(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     contact = models.CharField(max_length=200, null=False, blank=False)
+
+    def delete(self, *args, **kwargs):
+        for img in self.images.all():
+            img.delete()
+        
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -24,6 +31,23 @@ class ChepareImages(models.Model):
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name="images")
     chepare_type = models.CharField(max_length=20, choices=CHEPARE_TYPES)
     image = models.ImageField(upload_to=image_directory_distributor)
+
+    def delete(self, *args, **kwargs):
+        if self.image:
+            path = self.image.path
+
+            self.image.delete(save=False)
+            
+            curr_dir = os.path.dirname(path)
+            sellers_dir = os.path.join(settings.MEDIA_ROOT, 'sellers')
+            while curr_dir != sellers_dir and curr_dir != settings.MEDIA_ROOT:
+                try:
+                    os.rmdir(curr_dir)
+                    curr_dir = os.path.dirname(curr_dir)
+                except (OSError, ValueError):
+                    break
+                    
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         return f"{self.seller.name} - {self.chepare_type}"
