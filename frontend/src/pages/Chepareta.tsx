056c6dc4bc78eee53ba6c436fcb87703f2153fc7
Tamer <tamerchoyy@gmail.com>
2026-01-09 18:13:07 +0200

    refactor: Removed setDocTitle(). Now using custom-hook instead.

--- frontend/src/pages/Chepareta.tsx
+++ frontend/src/pages/Chepareta.tsx
@@ -1,41 +1,51 @@
 import { useState } from "react";
 import { Plus } from "lucide-react";
-import setDocTitle from "@/util/setDocTitle";
 import { useAuth } from "@/context/AuthContext";
 import { Button } from "@/components/ui/button";
 import { CreateChepareSellerDialog } from "@/components/dialogs/CreateChepareSellerDialog";
 import CheparetaCards from "@/components/Chepareta/CheparetaCards";
+import { useDocumentTitleChange } from "@/hooks/use-document-title-change";
+import { DOMAIN } from "@/util/constants";
 
 export default function Chepareta() {
   const { user } = useAuth();
   const [formOpen, setFormOpen] = useState(false);
-
-  setDocTitle("Fish Spots");
+  useDocumentTitleChange(`Чепарета | ${DOMAIN}`);
 
   return (
-    <main className="py-6">
-      {user?.isAdmin && (
-        <CreateChepareSellerDialog open={formOpen} onOpenChange={setFormOpen} />
-      )}
+    <>
+      <meta
+        name="description"
+        content="Намерете вашият майстор на чепарета за сафрид, карагьоз, паламуд и чернокоп!"
+      />
 
-      <div className="mx-auto mb-16 max-w-7xl">
-        <section className="relative flex items-center justify-center">
-          <h2 className="text-center text-3xl font-medium">Майстори</h2>
-          {user?.isAdmin && (
-            <div className="absolute right-0">
-              <Button
-                variant="ghost"
-                size="icon"
-                onClick={() => setFormOpen(true)}
-              >
-                <Plus />
-              </Button>
-            </div>
-          )}
-        </section>
+      <main className="py-6">
+        {user?.isAdmin && (
+          <CreateChepareSellerDialog
+            open={formOpen}
+            onOpenChange={setFormOpen}
+          />
+        )}
 
-        <CheparetaCards />
-      </div>
-    </main>
+        <div className="mx-auto mb-16 max-w-7xl">
+          <section className="relative flex items-center justify-center">
+            <h2 className="text-center text-3xl font-medium">Майстори</h2>
+            {user?.isAdmin && (
+              <div className="absolute right-0">
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => setFormOpen(true)}
+                >
+                  <Plus />
+                </Button>
+              </div>
+            )}
+          </section>
+
+          <CheparetaCards />
+        </div>
+      </main>
+    </>
   );
 }
