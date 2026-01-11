90eddeedeaa3f7f1bdf0a2a04a3da55a1433360b
Tamer <tamerchoyy@gmail.com>
2026-01-11 21:29:04 +0200

    refactor: updated because migration

--- backend/chepareta/utils.py
+++ backend/chepareta/utils.py
@@ -1,2 +1,2 @@
 def image_directory_distributor(instance, filename):
-    return f"sellers/{instance.seller.name}_{instance.seller.pk}/chepareta/{instance.chepare_type}/{filename}"
+    return f"sellers/{instance.seller.first_name}_{instance.seller.pk}/chepareta/{instance.chepare_type}/{filename}"
