a796e9dd65d5f5f3b75b63e8327ba69bb56dc821
Tamer <tamerchoyy@gmail.com>
2026-01-19 23:33:36 +0200

    refactor: Moved base.urls routes to users.urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from .views import (
-    UserExistsView,
+    CookieTokenRefreshView,
     cookie_consent_view,
-    google_login
+    google_login,
+    user_me_view,
+    logout_view,
 )
 
 urlpatterns = (
-    path("user-exists/", UserExistsView.as_view(), name="user_exists"),
+    path("me/", user_me_view, name="user_me_view"),
     path("auth/login/", google_login, name="google_login"),
+    path("token/refresh/", CookieTokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", logout_view, name="logout_view"),
     path("cookie-consent/", cookie_consent_view, name="cookie_consent"),
 )
