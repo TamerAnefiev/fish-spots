7db90a7fd301939c5958cc409a8ccde65049fbb4
Tamer <tamerchoyy@gmail.com>
2026-01-19 20:50:24 +0200

    refactor: Changed view name

--- backend/catches_history/urls.py
+++ backend/catches_history/urls.py
@@ -4,12 +4,12 @@ from .views import (
     catch_stats,
     get_oldest_catch_year,
     delete_catch_history,
-    CreateCatchHistory,
+    create_catch_history,
 )
 
 urlpatterns = (
     path("", catch_stats, name="catch_stats"),
     path("oldest-catch-year/", get_oldest_catch_year, name="oldest_catch_year"),
-    path("create/", CreateCatchHistory.as_view(), name="create_catch_history"),
+    path("create/", create_catch_history, name="create_catch_history"),
     path("delete/<int:pk>/", delete_catch_history, name="delete_catch_history"),
 )
