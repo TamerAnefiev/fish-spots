58447750c05250f86fb64d0d9aacc49ce4c94117
Tamer <tamerchoyy@gmail.com>
2026-01-19 20:53:44 +0200

    feat: Added google related columns

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,8 +4,14 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=255, null=False, blank=False, unique=True, error_messages={"unique": "Потребителското име е заето."})
-    email = models.EmailField(unique=True, null=False, blank=False, error_messages={"unique": "Имейл адресът е зает."})
+    username = models.CharField(max_length=255, unique=True, error_messages={"unique": "Потребителското име е заето."})
+    email = models.EmailField(unique=True, error_messages={"unique": "Имейл адресът е зает."})
+
+    google_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    picture = models.URLField(max_length=500, null=True, blank=True)
+
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
@@ -13,6 +19,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
+
+    def save(self, *args, **kwargs):
+        if self.email:
+            # to keep them synced automatically. Basically i dont need the username if i use google login
+            self.username = self.email
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.username
