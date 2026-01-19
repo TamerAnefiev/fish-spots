84928629d2f672afe385f46f34e27a38188f93ab
Tamer <tamerchoyy@gmail.com>
2026-01-19 23:33:52 +0200

    refactor: Moved base.utils into users.utils

--- backend/users/utils.py
+++ backend/users/utils.py
@@ -91,3 +91,19 @@ def get_or_create_user(user_info):
         user.save(update_fields=["picture"])
 
     return user
+
+
+def set_token_in_cookie(
+    response,
+    key: str,
+    value: str,
+    max_age: int,
+    secure=True,
+    httponly=True,
+    samesite="None",
+):
+    response.set_cookie(
+        key, value, max_age=max_age, secure=secure, httponly=httponly, samesite=samesite
+    )
+
+    return response
