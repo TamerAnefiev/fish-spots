1caac014e48a27c94f003c542252fecb8afab914
Tamer <tamerchoyy@gmail.com>
2026-01-19 20:57:22 +0200

    feat: Added google login endpoint and added names to the endpoints

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from .views import (
     UserExistsView,
-    EditUserView,
-    EditUserPasswordView,
     cookie_consent_view,
+    google_login
 )
 
 urlpatterns = (
-    path("user-exists/", UserExistsView.as_view()),
-    # path("edit/<username>", EditUserView.as_view()),
-    # path("edit-password/<username>", EditUserPasswordView.as_view()),
-    path("cookie-consent/", cookie_consent_view),
+    path("user-exists/", UserExistsView.as_view(), name="user_exists"),
+    path("auth/login/", google_login, name="google_login"),
+    path("cookie-consent/", cookie_consent_view, name="cookie_consent"),
 )
