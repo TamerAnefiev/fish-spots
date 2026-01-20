6d4ecba478626d9e830b637ce1e14be35824a717
Tamer <tamerchoyy@gmail.com>
2026-01-20 21:20:01 +0200

    refactor: Removed unused serializers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,31 +5,6 @@ from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 
-class BaseUserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = UserModel
-        fields = "__all__"
-
-
-class UserExistsData(BaseUserSerializer):
-    number_of_created_landscapes = serializers.SerializerMethodField()
-
-    class Meta:
-        model = UserModel
-        fields = ("username", "email", "number_of_created_landscapes")
-
-    def get_number_of_created_landscapes(self, user):
-        return 0
-
-
-class EditUserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = UserModel
-        fields = ("username", "email", "password")
-
-
 class UserMeSerializer(serializers.ModelSerializer):
     isAdmin = serializers.BooleanField(source="is_superuser")
 
