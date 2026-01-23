8141cc144143a1a7ad8abaca1fcbc9611afeaee4
Tamer <tamerchoyy@gmail.com>
2026-01-23 15:18:55 +0200

    feat: Added the user's picture if he is logged in

--- frontend/src/components/Navigation/UserDropdown.tsx
+++ frontend/src/components/Navigation/UserDropdown.tsx
@@ -8,17 +8,21 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { ThemeOptions } from "./ThemeOptions";
 import { UserOptions } from "./UserOptions";
+import { useAuth } from "@/context/AuthContext";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export function UserDropdown() {
+  const { user } = useAuth();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button
-          size={"icon"}
-          variant="ghost"
-          className="flex h-8 w-8 focus-visible:ring-0 focus-visible:ring-offset-0"
-        >
-          <User />
+        <Button size={"icon"} variant="ghost" className="rounded-full">
+          <Avatar className="size-8">
+            <AvatarImage src={user?.picture} alt="User profile" />
+            <AvatarFallback delayMs={50} className="bg-muted">
+              <User />
+            </AvatarFallback>
+          </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
